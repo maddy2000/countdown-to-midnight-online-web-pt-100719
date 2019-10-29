78bb7eabd3d2b689403221728e1a3529d898e80c
Madelyn Deshield <madelynwallop@gmail.com>
2019-10-29 03:30:23 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown (countdown)
+  while countdown > 0
+  puts "#{countdown} SECOND(S)!"
+  countdown-=1 
+ 
+  
+end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (countdown)
+  while countdown > 0 
+  sleep(1)
+  puts "#{countdown} SECONDS(S)!"
+  countdown-=1 
+  
+end
 end
